a3e1bc08cd29e7f07348829868b340da3677a292
HiWoo <allen.woo93@qq.com>
2019-07-01 14:38:01 +0800

    Updated: update coll

--- kill.py
+++ kill.py
@@ -2,7 +2,7 @@ import os
 
 __author__ = 'Allen Woo'
 try:
-    shcmd = """ps -ef | grep ups_web/uwsgi.ini | awk '{print $2}' | xargs kill -9"""
+    shcmd = """ps -ef | grep uwsgi.ini | awk '{print $2}' | xargs kill -9"""
     r = os.system(shcmd)
     print("Kill uwsgi.")
 except Exception as e:
